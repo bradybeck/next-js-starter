fcf38a8ae17341ae99000167c1304ab68bca9f86
Brady Becker <bradybeck@gmail.com>
2017-04-07 23:02:32 -0500

    cleaning up repo

--- Readme.md
+++ Readme.md
@@ -4,4 +4,4 @@ A repo consuming https://github.com/zeit/next.js.
 #### Motivation
 Speed up everyday workflow creating SSR apps with next-js-starter.
 
-In the box you get support for sass and express.
+In the box you get support for sass, express, and [bulma.io](http://bulma.io/).
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,18 @@ export default class extends React.Component {
 		return (
 			<div>
 				{getHeader()}
-				<h2> Bulma Spike </h2>
+				<section className="hero is-default is-fullheight">
+					<div className="hero-body">
+						<div className="container">
+							<h1 className="title">
+								Next JS Starter
+							</h1>
+							<h2 className="subtitle">
+								Take er easy.
+							</h2>
+						</div>
+					</div>
+				</section>
 			</div>
 		);
 	}
